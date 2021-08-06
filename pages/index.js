021b89768bf9ade8dfef65d4782bd1afd2b2a09f
Aidil <aidilrozi83@gmail.com>
2021-08-06 00:01:11 -0500

    Finished styling the header component.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Airbnb Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* Header */}
+      <Header />
       {/* Banner */}
     </div>
   );
