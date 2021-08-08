852375b0e240ec9bd4e606289d7e45ee880bb9e8
Aidil <aidilrozi83@gmail.com>
2021-08-08 14:04:10 -0500

    Finished styling MediumCard component and SmallCard component.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData, cardData }) {
   return (
     <div className="">
       <Head>
@@ -14,14 +16,40 @@ export default function Home() {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-
           {/* Pull some data from a server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <SmallCard key={item.img} item={item} />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardData?.map((item) => (
+              <MediumCard key={item.img} item={item} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
   );
 }
 
+// Static rendering
 export async function getStaticProps() {
-  const exploreData
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  const cardData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardData,
+    },
+  };
 }
