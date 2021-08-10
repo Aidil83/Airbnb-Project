74f650a132e5f0a1e653533e04466e680b4130af
Aidil <aidilrozi83@gmail.com>
2021-08-09 22:21:38 -0500

    Working on the routing.

--- components/Header.js
+++ components/Header.js
@@ -4,16 +4,20 @@ import {
   GlobeAltIcon,
   MenuIcon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/solid";
 import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 
 function Header() {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuest, setNoOfGuest] = useState(1);
+  const router = useRouter();
 
   const selectionRange = {
     startDate,
@@ -26,10 +30,29 @@ function Header() {
     setEndDate(ranges.selection.endDate);
   };
 
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const handleSearch = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuest,
+      },
+    });
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* ---------------------------------- Left --------------------------------- */}
-      <div className="relative flex items-center h-10 my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -60,13 +83,34 @@ function Header() {
         </div>
       </div>
       {searchInput && (
-        <div>
+        <div className="flex flex-col col-span-3 mx-auto mt-4">
           <DateRangePicker
             ranges={[selectionRange]}
             minDate={new Date()}
             rangeColors={["#FD5B61"]}
             onChange={handleSelect}
           />
+          <div className="flex items-center border-b">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuest}
+              onChange={(e) => setNoOfGuest(e.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex justify-around">
+            <button onClick={resetInput} className="text-gray-500">
+              Cancel
+            </button>
+            <button onClick={handleSearch} className="text-red-400">
+              Search
+            </button>
+          </div>
         </div>
       )}
     </header>
