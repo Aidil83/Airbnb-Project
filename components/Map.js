f9448ebc60d3501da36b844b7d31db509d1e2342
Aidil <aidilrozi83@gmail.com>
2021-08-11 22:59:26 -0500

    Finished with the project.

--- components/Map.js
+++ components/Map.js
@@ -3,6 +3,8 @@ import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import getCenter from "geolib/es/getCenter";
 
 function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   const coordinates = searchResults.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
@@ -28,7 +30,6 @@ function Map({ searchResults }) {
       onViewportChange={(viewport) => setViewport(viewport)}
     >
       {searchResults.map((result) => {
-        console.log("result ->", result);
         return (
           <div key={result.long}>
             <Marker
@@ -37,8 +38,28 @@ function Map({ searchResults }) {
               offsetLeft={-20}
               offsetRight={-10}
             >
-              <p className="cursor-pointer text-2xl animate-bounce">📌</p>
+              <p
+                role="img"
+                onClick={() => setSelectedLocation(result)}
+                className="cursor-pointer text-2xl animate-bounce"
+                aria-label="push-pin"
+              >
+                📌
+              </p>
             </Marker>
+            {/* The popup that should show if we click on a Marker */}
+            {selectedLocation.long === result.long ? (
+              <Popup
+                closeOnClick={true}
+                onClose={() => setSelectedLocation({})}
+                latitude={result.lat}
+                longitude={result.long}
+              >
+                {result.title}
+              </Popup>
+            ) : (
+              false
+            )}
           </div>
         );
       })}
